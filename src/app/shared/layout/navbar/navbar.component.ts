4f33f013d5afdec1249f3bedd396d924c25696d1
Bayoor <bayonleadebisijose@gmail.com>
2024-02-29 10:12:45 +0100

    fix the navbar responsiveness for mobile

--- src/app/shared/layout/navbar/navbar.component.ts
+++ src/app/shared/layout/navbar/navbar.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  menuOpen: boolean = true;
+  // menuClose: boolean = true;
+  
 
+  toggleMenu() {
+    
+    this.menuOpen = !this.menuOpen;
+    // this.menuOpen = !this.menuOpen;
+    
+  }
 }
