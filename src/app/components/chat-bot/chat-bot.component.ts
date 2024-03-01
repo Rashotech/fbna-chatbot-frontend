fd42ebbbe27994f91211b7e3854b91e904124842
Bayoor <bayonleadebisijose@gmail.com>
2024-03-01 10:32:54 +0100

    fix: the chatbot component

--- src/app/components/chat-bot/chat-bot.component.ts
+++ src/app/components/chat-bot/chat-bot.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { AboutComponent } from "../../pages/about/about.component";
 
 @Component({
-  selector: 'app-chat-bot',
-  standalone: true,
-  imports: [],
-  templateUrl: './chat-bot.component.html',
-  styleUrl: './chat-bot.component.css'
+    selector: 'app-chat-bot',
+    standalone: true,
+    templateUrl: './chat-bot.component.html',
+    styleUrl: './chat-bot.component.css',
+    imports: [AboutComponent]
 })
 export class ChatBotComponent {
+  isOpen: boolean = false;
 
+  openModal() {
+    this.isOpen = true;
+  }
+
+  closeModal() {
+    this.isOpen = false;
+  }
 }
