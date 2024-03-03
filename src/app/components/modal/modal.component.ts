23e2bcb374ae19ca128f58569296a148f816ff59
Rasheed Ayoade <rashotech@gmail.com>
2024-03-03 13:27:15 +0100

    integrate chat window

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,4 +1,12 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+declare global {
+  interface Window {
+    WebChat: any;
+  }
+}
+
+window.WebChat = window.WebChat || {};
 
 @Component({
   selector: 'app-modal',
@@ -7,10 +15,28 @@ import { Component, EventEmitter, Output } from '@angular/core';
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.css',
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
   @Output() closeIcon = new EventEmitter<boolean>();
 
   closeChatDialogModal() {
     this.closeIcon.emit(false);
+  }
+
+  ngOnInit(): void {
+    const script = document.createElement('script');
+    script.src = 'https://cdn.botframework.com/botframework-webchat/latest/webchat.js';
+    script.crossOrigin = 'anonymous';
+    script.onload = () => {
+      window.WebChat.renderWebChat(
+        {
+          directLine: window.WebChat.createDirectLine({
+            token: 'io7ROTvbgRg.-FKTgNLueqQIVo9pjDi6zvFwYwiWuiep6Ya_KCzmyQw'
+          }),
+          locale: 'en-US'
+        },
+        document.getElementById('webchat')
+      );
+    };
+    document.body.appendChild(script); 
   }
 }
