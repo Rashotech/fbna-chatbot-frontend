b7af9a1bf60d82719ea08adab3c4b5a41e6302d4
Rasheed Ayoade <rashotech@gmail.com>
2024-03-03 17:21:03 +0100

    feat: add styles

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -26,17 +26,29 @@ export class ModalComponent implements OnInit {
     const script = document.createElement('script');
     script.src = 'https://cdn.botframework.com/botframework-webchat/latest/webchat.js';
     script.crossOrigin = 'anonymous';
+    const styleOptions = {
+      bubbleBackground: 'rgba(0, 0, 255, .1)',
+      bubbleFromUserBackground: 'rgba(0, 255, 0, .1)',
+      botAvatarImage:
+        'https://firebasestorage.googleapis.com/v0/b/fbna-games.appspot.com/o/chatbot%2Fchatbot_dp.png?alt=media&token=b930abea-13de-42c2-950e-036864c60a99',
+      botAvatarInitials: 'CR',
+      userAvatarImage: 'https://github.com/compulim.png?size=64',
+      userAvatarInitials: 'WC'
+    };
     script.onload = () => {
       window.WebChat.renderWebChat(
         {
           directLine: window.WebChat.createDirectLine({
             token: 'io7ROTvbgRg.-FKTgNLueqQIVo9pjDi6zvFwYwiWuiep6Ya_KCzmyQw'
           }),
+          styleOptions,
           locale: 'en-US'
         },
-        document.getElementById('webchat')
+        document.getElementById('webchat'),
       );
+      (document.querySelector('#webchat > *') as HTMLElement)?.focus(); 
     };
+
     document.body.appendChild(script); 
   }
 }
